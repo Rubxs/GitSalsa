591edcd24c30dd178bc81733d7b4446616496931
geniscarmona <34104215+geniscarmona@users.noreply.github.com>
2020-02-06 19:05:02 +0000

    Delete installer.sh

--- components/installer.sh
+++
@@ -1,6 +0,0 @@
-#Filtro que se encarga de encontrar que distribucion Linux tienes instalado en tu equipo
-. ../functions/intallation.sh
-
-distlinux=$(cat  /etc/*-release | tail -n+7 | head -n-9| cut -f2 -d=)
-
-intall distlinux
