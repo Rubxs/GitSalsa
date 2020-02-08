4839aa91a63deaa3100fb030641ad6ba60073af2
Rubén Herreros <rubenherrerosmartin17@gmail.com>
2020-02-08 21:33:04 +0100

    Cambios históricos y errores
    Logs historic.log y error.log
    Funciones y componentes

--- functions/log.sh
+++ functions/log.sh
@@ -1,6 +1,20 @@
 # Autor: Rubén Herreros
-# Función: 
+# Funciones para la creación y gestión de los archivos LOG del programa
 
 function setLog() {
+    type=$1
+    id=$2
+    user=$3
+    action=$4
+    repository=$5
+    message=$6
 
+    if [ $type == 'error' ]
+        then
+            echo $id':'$user':'$action':'$repository':'$message >> /home/ruben/Escritorio/gitsalsa/databases/error.log
+        else
+            echo $id':'$user':'$action':'$repository':'$message >> /home/ruben/Escritorio/gitsalsa/databases/historic.log
+    fi
+
+    # Revisar las rutas absolutas al crear la estructura del programa
 }
