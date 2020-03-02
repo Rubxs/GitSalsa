b1b9d70a35bccca49be1a04ae15600e004acd4f6
geniscarmona <34104215+geniscarmona@users.noreply.github.com>
2020-03-02 21:10:36 +0100

    Update compr.sh

--- components/compr.sh
+++ components/compr.sh
@@ -1,19 +1,27 @@
 #Autor:Gines Carmona
 #Verificar si git y zenity estan instalados en sistema operativo
-ins=$(dpkg -l "git" | wc -l)
-ins2=$(dpkg -l zenity | wc -l)
+. ../functions/installer.sh
+zenity --question --text "Instalacion GitSalsa"
+comp=$(echo $?)
+function insta(){
+	if [[ $1 = "0" ]]; then
+		ins=$(dpkg -l "git" | wc -l)
+		ins2=$(dpkg -l zenity | wc -l)
+		if [ $ins2 = "0" ];
+			then 
+				echo `Installer zenity`
+			else 
+				echo Ya tienes instalado Zenity en tu equipo
+			fi
 
-if [ $ins = "0" ];
-then 
-	InstallGit()
-else 
-	echo Ya tienes instalado git en tu equipo
-fi
-
-if [ $ins2 = "0" ];
-then 
-	InstallZenity()
-else
-	echo Ya tienes instalado zenity en tu equipo
-
-fi
+			if [ $ins = "0" ];
+			then 
+				echo `Installer git`
+			else 
+			echo Ya tienes instalado git en tu equipo
+			fi
+		else
+			exit	
+	fi			
+}	
+insta $comp
